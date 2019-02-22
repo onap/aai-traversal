7faefd6b1bf7bafa0da52704f4d05d9fd789ceac
Arindam Mondal <arind.mondal@samsung.com>
2019-02-22 14:45:26 +0900

    Method should not have too many parameter
    Issue-ID: AAI-2187
    Change-Id: Ied22a0cacb53f37d768020c57c2026f229a1e94a
    Signed-off-by: Arindam Mondal <arind.mondal@samsung.com>

---
+++ aai-traversal/src/main/java/org/onap/aai/util/RunGenericQueryBuilder.java
@@ -0,0 +1,97 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Samsung Electronics Co., Ltd. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.util;
+
+import javax.ws.rs.core.HttpHeaders;
+import java.util.List;
+import org.onap.aai.introspection.Loader;
+import org.onap.aai.serialization.queryformats.utils.UrlBuilder;
+import org.onap.aai.serialization.engines.TransactionalGraphEngine;
+
+public class RunGenericQueryBuilder {
+
+	private HttpHeaders headers;
+	String startNodeType;
+	List <String> startNodeKeyParams;
+	List <String> includeNodeTypes;
+	private int depth;
+	TransactionalGraphEngine dbEngine;
+	Loader loader;
+	UrlBuilder urlBuilder;
+	
+	public HttpHeaders getHeaders() {
+		return headers;
+	}
+	public String getStartNodeType() {
+		return startNodeType;
+	}
+	public List<String> getStartNodeKeyParams() {
+		return startNodeKeyParams;
+	}
+	public List<String> getIncludeNodeTypes() {
+		return includeNodeTypes;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public TransactionalGraphEngine getDbEngine() {
+		return dbEngine;
+	}
+	public Loader getLoader() {
+		return loader;
+	}
+	public UrlBuilder getUrlBuilder() {
+		return urlBuilder;
+	}
+	
+	
+	public RunGenericQueryBuilder setHeaders(HttpHeaders headers) {
+		this.headers = headers;
+		return this;
+	}
+	public RunGenericQueryBuilder setStartNodeType(String startNodeType) {
+		this.startNodeType = startNodeType;
+		return this;
+	}
+	public RunGenericQueryBuilder setStartNodeKeyParams(List<String> startNodeKeyParams) {
+		this.startNodeKeyParams = startNodeKeyParams;
+		return this;
+	}
+	public RunGenericQueryBuilder setIncludeNodeTypes(List<String> includeNodeTypes) {
+		this.includeNodeTypes = includeNodeTypes;
+		return this;
+	}
+	public RunGenericQueryBuilder setDepth(int depth) {
+		this.depth = depth;
+		return this;
+	}
+	public RunGenericQueryBuilder setDbEngine(TransactionalGraphEngine dbEngine) {
+		this.dbEngine = dbEngine;
+		return this;
+	}
+	public RunGenericQueryBuilder setLoader(Loader loader) {
+		this.loader = loader;
+		return this;
+	}
+	public RunGenericQueryBuilder setUrlBuilder(UrlBuilder urlBuilder) {
+		this.urlBuilder = urlBuilder;
+		return this;
+	}
+}
